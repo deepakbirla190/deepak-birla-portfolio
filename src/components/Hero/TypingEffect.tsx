6266305fc04e72b03d6af9cb29e059fc637d0004
Deepak Birla <deepakbirla@zuzucodes.com>
2026-07-24 14:11:30 +0530

    remove-unused-dependency

--- src/components/Hero/TypingEffect.tsx
+++ src/components/Hero/TypingEffect.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 const TypingEffect = ({ words }: { words: string[] }) => {
   // const [wordIndex, setWordIndex] = useState(0)
